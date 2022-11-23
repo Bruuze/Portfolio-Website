ba69a9087ff6c9829f38c17bea84a5e5ba7da753
Bruuze <aidan.murphy.personal@gmail.com>
2022-11-23 01:39:40 +0000

    Update SocialForm.vue

--- components/SocialForm.vue
+++ components/SocialForm.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <form
+      id="myForm"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
       method="POST"
-      action="/contactform"
-      name="Contact" >
+      name="Contact" 
+      onSubmit={{handleSubmit}}>
 
       <input type="hidden" name="form-name" value="Contact" />
 
@@ -22,3 +23,27 @@
     </form>
   </div>
 </template>
+
+<script>
+function encode(data) {
+  return Object.keys(data)
+    .map(
+      (key) =>
+        encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+    )
+    .join("&");
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: encode({
+      "form-name": event.target.getAttribute("name"),
+      ...name,
+    }),
+  })
+    .catch((error) => alert(error));
+};
+</script>
