a52a2f6305c5c2746e9490ccf973ddc6c1f952e9
Bruuze <aidan.murphy.personal@gmail.com>
2022-11-23 02:34:21 +0000

    Update SocialForm.vue

--- components/SocialForm.vue
+++ components/SocialForm.vue
@@ -7,27 +7,31 @@
       netlify 
       netlify-honeypot="bot-field" 
       data-netlify="true" 
-      @submit.prevent="onFormSubmit"
+      @submit.prevent="incrementCounter"
     >
       <input type="hidden" name="form-name" value="subscribe">
       <input type="email" name="email" required>
-      <button @click="onFormSubmit">Submit</button>
+      <button>Submit</button>
     </form>
   </div>
 </template>
 
 
 <script>
-const onFormSubmit = (e) => {
-  console.log('worked');
-  let myForm = document.getElementById("subscribe");
-  let formData = new FormData(myForm);
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-  .then(console.log(r))
-  .catch((error) => console.log(error));
+export default{
+  methods: {
+    incrementCounter() {
+      console.log('worked');
+      let myForm = document.getElementById("subscribe");
+      let formData = new FormData(myForm);
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+      })
+      .then(console.log('it worked'))
+      .catch((error) => console.log(error));
+    }
+  }
 }
 </script>
