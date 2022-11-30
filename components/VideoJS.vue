c08a8cafe3ed5f53d573e2420294851871566b97
Bruuze <aidan.murphy.personal@gmail.com>
2022-11-30 17:40:18 +0000

    New Video component

--- components/VideoJS.vue
+++ components/VideoJS.vue
@@ -1,55 +1,37 @@
 <template>
-    <video-player
-    id="CustomVideoPlayer"
-    class="video-player vjs-theme-forest"
-    crossorigin="anonymous"
-    playsinline
-    controls
-    :src = "url"
-    :height="320"
-    :volume="0.4"
-    :children="[
-      // custom Video.js children component
-      // https://videojs.com/guides/options/#children
-      'mediaLoader',
-      'posterImage',
-      'bigPlayButton',
-      'loadingSpinner',
-      'controlBar',
-      'textTrackDisplay'
-    ]"
-    :control-bar="{
-      // custom Video.js control bar component
-      // https://videojs.com/guides/options/#componentname
-      volumePanel: false
-    }"
-    />
-  </template>
+  <div>
+    <video ref="videoPlayer" class="video-js vjs-custom-waiting" src="https://marvelous-hotteok-361980.netlify.app/uploads/EndingNow.mp4"></video>
+  </div>
+</template>
 
-  <script lang="ts">
-    import { defineComponent, shallowRef } from 'vue'
-    import { VideoJsPlayer } from 'video.js'
-    import { VideoPlayer } from '@videojs-player/vue'
-    import 'video.js/dist/video-js.css'
-    import '@videojs/themes/dist/city/index.css'
-    export default defineComponent({
-      name: 'vue-basic-player-example',
-      title: 'Basic player (Vue)',
-      url: import.meta.url,
-      components: {
-        VideoPlayer
+<script>
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css'
+export default {
+  name: 'VideoPlayer',
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {autoplay:true, width: 550, loop:true};
       },
-      setup() {
-        const player = shallowRef<VideoJsPlayer>()
-        const handleMounted = (payload: any) => {
-          player.value = payload.player
-          //console.log('Basic player mounted', payload)
-        }
-        const handleEvent = (log: any) => {
-          //console.log('Basic player event', log)
-        }
-        return { player, handleMounted, handleEvent }
-      },
-      props: ['url']
-    })
-  </script>
+    }
+  },
+  data() {
+    return {
+      player: null
+    }
+  },
+  mounted() {
+    this.player = videojs(this.$refs.videoPlayer, this.options, () => {
+      this.player.log('onPlayerReady', this);
+      $(".video-js").removeClass( "vjs-custom-waiting" )
+    });
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+}
+</script>
--- pages/portfolio/[...slug].vue
+++ pages/portfolio/[...slug].vue
@@ -2,7 +2,7 @@
 
 const { page } = useContent()
 const pageBody = useNuxtApp().$mdit.render(page.value.body)
-
+const interactivePreview = page.value.interactivePreview
 useHead({
   title: page.value.title,
   meta: [
@@ -16,12 +16,14 @@ useHead({
       <div class="containerPost">
         <TopNavWork/>
         <div oncontextmenu="return false;" v-html="pageBody"></div>
-        <div v-for="(preview,i) in page.interactivePreview">
-          <h3>{{preview.name}}</h3>
-          <p>{{preview.video}}</p>
-          <i>{{preview.tag}}</i>
-          <button @click="showVideo(i)">Show {{preview.name}}</button>
-          <LazyVideoJS :url="'https://marvelous-hotteok-361980.netlify.app/' + preview.video" v-if="show == i"/>
+        <button @click="showLinks('graphics')">Graphics</button> <button @click="showLinks('alerts')">Alerts</button>
+        <ul>
+          <li v-for="(preview,i) in interactivePreview" :class="['Button_'+preview.tag, {dontShow: preview.tag != 'graphics' }]">
+            <button @click="showVideo(i)">{{preview.name}}</button>
+          </li>
+        </ul>
+        <div v-for="(preview,i) in interactivePreview">
+          <LazyVideoJS :url="'http://localhost:3000/' + preview.video" v-if="show == i && activated"/>
         </div>
         <LatestWork />
         <SocialForm />
@@ -33,13 +35,45 @@ useHead({
 export default{
   data(){
     return{
-      show: 0
+      show: 0,
+      activated: false,
     }
   },
   methods: {
     showVideo(id){
       this.show = id
+      this.activated = true
+    },
+    showLinks(id){
+      if(id == "graphics"){
+        $(".Button_alerts").addClass( "dontShow" )
+        $(".Button_graphics").removeClass( "dontShow" )
+        //$(".vjs-loading-spinner").addClass( ".vjs-custom-waiting" )
+      } else{
+        $(".Button_graphics").addClass( "dontShow" )
+        $(".Button_alerts").removeClass( "dontShow" )
+        //$(".vjs-loading-spinner").addClass( ".vjs-custom-waiting" )
+      }
     }
   }
 }
 </script>
+
+<style>
+.dontShow{
+  display: none !important;
+}
+
+.vjs-custom-waiting .vjs-loading-spinner
+{
+    display: block;
+    visibility: visible;
+}
+.video-js.vjs-custom-waiting .vjs-loading-spinner:before,
+.video-js.vjs-custom-waiting .vjs-loading-spinner:after
+{
+    /* I just copied the same animation as in the default css file */
+    -webkit-animation: vjs-spinner-spin 1.1s cubic-bezier(0.6, 0.2, 0, 0.8) infinite, vjs-spinner-fade 1.1s linear infinite;
+    animation: vjs-spinner-spin 1.1s cubic-bezier(0.6, 0.2, 0, 0.8) infinite, vjs-spinner-fade 1.1s linear infinite;
+}
+</style>
